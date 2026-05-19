8c75085b19658d84b3011a96970cf35b3f93c87b
tonusridey <150463205+tonusridey@users.noreply.github.com>
2026-05-19 16:33:03 +0600

    Add temperature check with print statements

---
+++ Task 1/youDo6.py
@@ -0,0 +1,10 @@
+temperature = 43
+
+if temperature >= 30:
+    print("It is hot")
+    
+elif 20 <= temperature <= 30:
+    print("It is nice")
+    
+elif temperature < 20:
+    print("It is cold")

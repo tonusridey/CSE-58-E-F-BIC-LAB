ad5a473182a8370465340b694cdc84426f3fe9cd
tonusridey <150463205+tonusridey@users.noreply.github.com>
2026-06-09 16:22:45 +0600

    Implement reverse complement function for DNA
    Fix the r_c function to correctly compute the reverse complement of a DNA sequence.

--- Task 2/Problem3.py
+++ Task 2/Problem3.py
@@ -1,1 +1,14 @@
+def r_c(pattern):
+    complement = { 'A':d'T','T' : 'A', 'C' : 'G','G' : 'c' }
 
+    result = ""
+
+    for char in pattern:
+        c string[::-1]
+ pattern_input = input().strip()
+
+result = r_c(pattern_input)   
+print(result) 
+
+
+    
